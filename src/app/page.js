eeca5b4cd1f61e3be69d8417270570aaf6c90d2c
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-08-01 23:50:48 +0530

    Adding Rest NextJs Project

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold text-center">
+        Welcome to My Plan My Trip App
+      </h1>
+    </div>
+  );
+}
